c961385a4a12612610bee76d6a4619499f0b0407
Artemeleon Morarevich <artyommorar@gmail.com>
2025-10-07 10:02:19 +0300

    final test version

--- Array_Template/Array.h
+++ Array_Template/Array.h
@@ -5,17 +5,17 @@ using namespace std;
 template <class T>
 class Array
 {
-    T* arr;
-    int size;
-    int realUsed;
-    int growStep;
+    T* data;
+    int capacity;
+    int count;
+    int growthStep;
 public:
     Array();
-    Array(int size);
-    Array(const T* mas, int size);
+    Array(int initialCapacity);
+    Array(const T* source, int sourceCount);
     void Print();
     int GetSize() const;
-    void SetSize(int newSize, int grow = 1);
+    void SetSize(int newCapacity, int grow = 1);
     ~Array();
     Array(const Array& other);
     Array& operator=(const Array& other);
@@ -32,85 +32,99 @@ public:
     const T& operator[](int index) const;
 
     void FreeExtra();
-
     void RemoveAll();
 
     int Add(const T& value);
 
-    void InsertAt(int pos, const T& value);
-    void RemoveAt(int pos);
+    void InsertAt(int position, const T& value);
+    void RemoveAt(int position);
 
     void Append(const Array& other);
 
     T* GetData();
     const T* GetData() const;
-
-
 };
 
 template <class T>
-Array<T>::Array() : arr(nullptr), size(0), realUsed(0), growStep(1) {}
+Array<T>::Array() : data(nullptr), capacity(0), count(0), growthStep(1) {}
 
 template <class T>
-Array<T>::Array(int size)
+Array<T>::Array(int initialCapacity)
 {
-    this->size = size;
-    realUsed = 0;
-    growStep = 1;
-    if (this->size > 0)
+    if (initialCapacity < 0)
     {
-        arr = new T[this->size];
+        initialCapacity = 0;
+    }
+
+    capacity = initialCapacity;
+    count = 0;
+    growthStep = 1;
+
+    if (capacity > 0)
+    {
+        data = new T[capacity]{};
     }
     else
     {
-        arr = nullptr;
-        this->size = 0;
+        data = nullptr;
+        capacity = 0;
     }
 }
 
 template <class T>
-Array<T>::Array(const T* mas, int size)
+Array<T>::Array(const T* source, int sourceCount)
 {
-    growStep = 1;
-    if (size < 0)
+    growthStep = 1;
+
+    if (sourceCount < 0)
     {
-        size = 0;
+        sourceCount = 0;
     }
-    this->size = size;
-    if (this->size > 0)
+
+    capacity = sourceCount;
+
+    if (capacity > 0)
     {
-        arr = new T[this->size];
+        data = new T[capacity]{};
     }
     else
     {
-        arr = nullptr;
-        this->size = 0;
+        data = nullptr;
+        capacity = 0;
     }
-    realUsed = this->size;
-    for (int i = 0; i < realUsed; i = i + 1)
+
+    count = capacity;
+
+    if (source != nullptr)
     {
-        arr[i] = mas[i];
+        for (int i = 0; i < count; i = i + 1)
+        {
+            data[i] = source[i];
+        }
     }
 }
 
 template <class T>
 Array<T>::Array(const Array& other)
 {
-    size = other.size;
-    realUsed = other.realUsed;
-    growStep = other.growStep;
-    if (size > 0)
+    capacity = other.capacity;
+    count = other.count;
+    growthStep = other.growthStep;
+
+    if (capacity > 0)
     {
-        arr = new T[size];
+        data = new T[capacity]{};
     }
     else
     {
-        arr = nullptr;
+        data = nullptr;
     }
-    for (int i = 0; i < realUsed; i = i + 1)
+
+    for (int i = 0; i < count; i = i + 1)
     {
-        arr[i] = other.arr[i];
+        data[i] = other.data[i];
     }
+
     cout << "Copy constructor!" << endl;
 }
 
@@ -120,23 +134,28 @@ Array<T>& Array<T>::operator=(const Array& other)
     if (this != &other)
     {
         T* tmp = nullptr;
-        if (other.size > 0)
+
+        if (other.capacity > 0)
         {
-            tmp = new T[other.size];
+            tmp = new T[other.capacity]{};
         }
-        for (int i = 0; i < other.realUsed; i = i + 1)
+
+        for (int i = 0; i < other.count; i = i + 1)
         {
-            tmp[i] = other.arr[i];
+            tmp[i] = other.data[i];
         }
-        if (arr != nullptr)
+
+        if (data != nullptr)
         {
-            delete[] arr;
+            delete[] data;
         }
-        arr = tmp;
-        size = other.size;
-        realUsed = other.realUsed;
-        growStep = other.growStep;
+
+        data = tmp;
+        capacity = other.capacity;
+        count = other.count;
+        growthStep = other.growthStep;
     }
+
     cout << "Copy= operator!" << endl;
     return *this;
 }
@@ -144,9 +163,9 @@ Array<T>& Array<T>::operator=(const Array& other)
 template <class T>
 void Array<T>::Print()
 {
-    for (int i = 0; i < realUsed; i = i + 1)
+    for (int i = 0; i < count; i = i + 1)
     {
-        cout << arr[i] << "\t";
+        cout << data[i] << "\t";
     }
     cout << endl;
 }
@@ -154,60 +173,319 @@ void Array<T>::Print()
 template <class T>
 int Array<T>::GetSize() const
 {
-    return size;
+    return capacity;
 }
 
 template <class T>
-void Array<T>::SetSize(int newSize, int grow)
+void Array<T>::SetSize(int newCapacity, int grow)
 {
-    if (newSize < 0)
+    if (newCapacity < 0)
     {
-        newSize = 0;
+        newCapacity = 0;
     }
     if (grow <= 0)
     {
         grow = 1;
     }
-    if (newSize != size)
+
+    if (newCapacity != capacity)
     {
-        T* newArr = nullptr;
-        if (newSize > 0)
+        T* newData = nullptr;
+
+        if (newCapacity > 0)
         {
-            newArr = new T[newSize];
+            newData = new T[newCapacity]{};
         }
+
         int toCopy;
-        if (realUsed < newSize)
+
+        if (count < newCapacity)
         {
-            toCopy = realUsed;
+            toCopy = count;
         }
         else
         {
-            toCopy = newSize;
+            toCopy = newCapacity;
         }
+
         for (int i = 0; i < toCopy; i = i + 1)
         {
-            newArr[i] = arr[i];
+            newData[i] = data[i];
         }
-        if (arr != nullptr)
+
+        if (data != nullptr)
         {
-            delete[] arr;
+            delete[] data;
         }
-        arr = newArr;
-        size = newSize;
-        realUsed = toCopy;
+
+        data = newData;
+        capacity = newCapacity;
+        count = toCopy;
     }
-    growStep = grow;
+
+    growthStep = grow;
 }
 
 template <class T>
 Array<T>::~Array()
 {
-    if (arr != nullptr)
+    if (data != nullptr)
     {
-        delete[] arr;
+        delete[] data;
     }
-    arr = nullptr;
-    size = 0;
-    realUsed = 0;
-    growStep = 1;
+
+    data = nullptr;
+    capacity = 0;
+    count = 0;
+    growthStep = 1;
+}
+
+template <class T>
+bool Array<T>::IsEmpty() const
+{
+    if (count == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+template <class T>
+int Array<T>::GetUpperBound() const
+{
+    if (count == 0)
+    {
+        return -1;
+    }
+    else
+    {
+        return count - 1;
+    }
+}
+
+template <class T>
+T& Array<T>::GetAt(int index)
+{
+    if (index >= 0 && index < count)
+    {
+        return data[index];
+    }
+
+    cout << "Index out of range" << endl;
+    static T defaultValue = T();
+    return defaultValue;
+}
+
+template <class T>
+const T& Array<T>::GetAt(int index) const
+{
+    if (index >= 0 && index < count)
+    {
+        return data[index];
+    }
+
+    cout << "Index out of range" << endl;
+    static T defaultValue = T();
+    return defaultValue;
+}
+
+template <class T>
+void Array<T>::SetAt(int index, const T& value)
+{
+    if (index >= 0 && index < count)
+    {
+        data[index] = value;
+    }
+}
+
+template <class T>
+T& Array<T>::operator[](int index)
+{
+    return GetAt(index);
+}
+
+template <class T>
+const T& Array<T>::operator[](int index) const
+{
+    return GetAt(index);
+}
+
+template <class T>
+void Array<T>::FreeExtra()
+{
+    if (capacity > count)
+    {
+        T* newData = nullptr;
+
+        if (count > 0)
+        {
+            newData = new T[count]{};
+        }
+
+        for (int i = 0; i < count; i = i + 1)
+        {
+            newData[i] = data[i];
+        }
+
+        if (data != nullptr)
+        {
+            delete[] data;
+        }
+
+        data = newData;
+        capacity = count;
+    }
+}
+
+template <class T>
+void Array<T>::RemoveAll()
+{
+    if (data != nullptr)
+    {
+        delete[] data;
+    }
+
+    data = nullptr;
+    capacity = 0;
+    count = 0;
+}
+
+template <class T>
+int Array<T>::Add(const T& value)
+{
+    if (count == capacity)
+    {
+        int newCapacity = capacity + growthStep;
+        if (newCapacity <= capacity)
+        {
+            newCapacity = capacity + 1;
+        }
+        SetSize(newCapacity, growthStep);
+    }
+
+    data[count] = value;
+    count = count + 1;
+    return count - 1;
+}
+
+template <class T>
+void Array<T>::InsertAt(int position, const T& value)
+{
+    if (position < 0)
+    {
+        return;
+    }
+    if (position > count)
+    {
+        return;
+    }
+
+    if (count == capacity)
+    {
+        int newCapacity = capacity + growthStep;
+        if (newCapacity <= capacity)
+        {
+            newCapacity = capacity + 1;
+        }
+        SetSize(newCapacity, growthStep);
+    }
+
+    for (int i = count; i > position; i = i - 1)
+    {
+        data[i] = data[i - 1];
+    }
+
+    data[position] = value;
+    count = count + 1;
+}
+
+template <class T>
+void Array<T>::RemoveAt(int position)
+{
+    if (position < 0)
+    {
+        return;
+    }
+    if (position >= count)
+    {
+        return;
+    }
+
+    for (int i = position; i < count - 1; i = i + 1)
+    {
+        data[i] = data[i + 1];
+    }
+
+    count = count - 1;
+}
+
+template <class T>
+void Array<T>::Append(const Array& other)
+{
+    if (other.count == 0)
+    {
+        return;
+    }
+
+    if (this == &other)
+    {
+        int itemsToCopy = count;
+
+        if (itemsToCopy == 0)
+        {
+            return;
+        }
+
+        T* tmp = new T[itemsToCopy]{};
+
+        for (int i = 0; i < itemsToCopy; i = i + 1)
+        {
+            tmp[i] = data[i];
+        }
+
+        int required = count + itemsToCopy;
+
+        if (required > capacity)
+        {
+            SetSize(required, growthStep);
+        }
+
+        for (int i = 0; i < itemsToCopy; i = i + 1)
+        {
+            data[count + i] = tmp[i];
+        }
+
+        count = count + itemsToCopy;
+        delete[] tmp;
+        return;
+    }
+
+    int required = count + other.count;
+
+    if (required > capacity)
+    {
+        SetSize(required, growthStep);
+    }
+
+    for (int i = 0; i < other.count; i = i + 1)
+    {
+        data[count + i] = other.data[i];
+    }
+
+    count = count + other.count;
+}
+
+template <class T>
+T* Array<T>::GetData()
+{
+    return data;
+}
+
+template <class T>
+const T* Array<T>::GetData() const
+{
+    return data;
 }
--- Array_Template/Main.cpp
+++ Array_Template/Main.cpp
@@ -1,25 +1,91 @@
 #include <iostream>
-#include <time.h>
 #include "Array.h"
 using namespace std;
 
+void Section(const char* title)
+{
+    cout << "\n" << title << "\n";
+}
+
 int main()
 {
-	srand(time(0));
+    Section("ctor default + IsEmpty");
+    Array<int> a;
+    if (a.IsEmpty())
+    {
+        cout << "a is empty\n";
+    }
+    cout << "capacity = " << a.GetSize() << ", upper = " << a.GetUpperBound() << endl;
 
-	int mas[3]{1,2,3};
+    Section("SetSize + Add");
+    a.SetSize(3, 2);
+    cout << "capacity = " << a.GetSize() << endl;
+    a.Add(10);
+    a.Add(20);
+    a.Add(30);
+    a.Print();
+    a.Add(40); // три места было, добавление 4-го проверит рост
+    cout << "after Add, capacity = " << a.GetSize()
+        << ", upper = " << a.GetUpperBound() << endl;
+    a.Print();
 
-	Array<int> object1;
-	object1.Print();
+    Section("operator[] / GetAt / SetAt");
+    cout << "a[1] = " << a[1] << endl;
+    cout << "GetAt(2) = " << a.GetAt(2) << endl;
+    a.SetAt(1, 777);
+    a.Print();
 
-	Array<int> object2 = object1;
-	object2.Print();
+    Section("InsertAt / RemoveAt");
+    a.InsertAt(0, 5);
+    a.InsertAt(3, 55);
+    a.Print();
+    a.RemoveAt(2);
+    a.Print();
 
-	Array<int> object3;
-	object3 = object2;
-	object3.Print();
+    Section("FreeExtra");
+    a.SetSize(a.GetSize() + 5, 2);
+    cout << "capacity increased to " << a.GetSize() << endl;
+    a.FreeExtra();
+    cout << "capacity after FreeExtra = " << a.GetSize() << endl;
 
+    Section("Append + copy/assign");
+    int src[] = { 1, 2, 3, 4 };
+    Array<int> b(src, 4);
+    Array<int> c;
+    c.Append(b);
+    c.Print();
 
+    Array<int> d = b;  // copy ctor
+    d.Print();
 
-	return 0;
+    Array<int> e(1);
+    e = b;             // operator=
+    e.Print();
+
+    Section("GetData");
+    int* p = c.GetData();
+    if (p != nullptr)
+    {
+        cout << "first of c = " << p[0] << endl;
+    }
+
+    Section("RemoveAll");
+    c.RemoveAll();
+    cout << "c capacity = " << c.GetSize() << ", upper = " << c.GetUpperBound() << endl;
+    if (c.IsEmpty())
+    {
+        cout << "c is empty\n";
+    }
+
+    Section("Array<double>");
+    Array<double> ad;
+    ad.SetSize(2, 1);
+    ad.Add(3.14);
+    ad.Add(2.71);
+    ad.InsertAt(1, 1.11);
+    ad.Print();
+    ad.FreeExtra();
+
+    cout << "\nDone\n";
+    return 0;
 }
